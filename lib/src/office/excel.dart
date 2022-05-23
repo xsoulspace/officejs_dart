012248f36baa541da1e70aa1124f66da01dd4250
Anton Malofeev <anton@xsoulspace.dev>
2022-05-24 00:47:29 +0300

    feat: expandos

--- lib/src/office/excel.dart
+++ lib/src/office/excel.dart
@@ -13,58 +13,105 @@ class Excel {
     if (_context != null) return _context!;
 
     final jsContext = await handleThenable(runExcelJsImpl());
-    _context = RequestContext.fromJsObject(jsContext);
+    _context = RequestContext.getInstance(jsContext);
     return _context!;
   }
 }
 
 class RequestContext extends JsObjectWrapper<excel_js.RequestContextJsImpl> {
-  RequestContext.fromJsObject(super.jsObject);
-  Workbook get workbook => Workbook.fromJsObject(super.jsObject.workbook);
+  RequestContext._fromJsObject(super.jsObject);
+
+  /// Creates a [RequestContext] from a [jsObject].
+  ///
+  /// {@template expando_explanation}
+  /// If an instance is already associated with [jsObject],
+  /// it is returned instead of creating a new instance.
+  /// {@endtemplate}
+  factory RequestContext.getInstance(
+    final excel_js.RequestContextJsImpl jsObject,
+  ) {
+    return _expando[jsObject] ??= RequestContext._fromJsObject(jsObject);
+  }
+
+  static final _expando = Expando<RequestContext>();
+
+  Workbook get workbook => Workbook.getInstance(super.jsObject.workbook);
 }
 
 class Workbook extends JsObjectWrapper<excel_js.WorkbookJsImpl> {
-  Workbook.fromJsObject(super.jsObject);
+  Workbook._fromJsObject(super.jsObject);
+
+  /// Creates a [Workbook] from a [jsObject].
+  ///
+  /// {@macro expando_explanation}
+  factory Workbook.getInstance(
+    final excel_js.WorkbookJsImpl jsObject,
+  ) {
+    return _expando[jsObject] ??= Workbook._fromJsObject(jsObject);
+  }
+
+  static final _expando = Expando<Workbook>();
 
   RequestContext get context =>
-      RequestContext.fromJsObject(super.jsObject.context);
+      RequestContext.getInstance(super.jsObject.context);
 
   WorksheetCollection get worksheets =>
-      WorksheetCollection.fromJsObject(super.jsObject.worksheets);
+      WorksheetCollection.getInstance(super.jsObject.worksheets);
 
   Worksheet load(final List<String> propertyNames) =>
-      Worksheet.fromJsObject(super.jsObject.load(propertyNames));
+      Worksheet.getInstance(super.jsObject.load(propertyNames));
 }
 
 class WorksheetCollection
     extends JsObjectWrapper<excel_js.WorksheetCollectionJsImpl> {
-  WorksheetCollection.fromJsObject(super.jsObject);
+  WorksheetCollection._fromJsObject(super.jsObject);
+
+  /// Creates a [WorksheetCollection] from a [jsObject].
+  ///
+  /// {@macro expando_explanation}
+  factory WorksheetCollection.getInstance(
+    final excel_js.WorksheetCollectionJsImpl jsObject,
+  ) {
+    return _expando[jsObject] ??= WorksheetCollection._fromJsObject(jsObject);
+  }
+
+  static final _expando = Expando<WorksheetCollection>();
 
   RequestContext get context =>
-      RequestContext.fromJsObject(super.jsObject.context);
+      RequestContext.getInstance(super.jsObject.context);
 
   List<Worksheet> get items =>
-      super.jsObject.items.map(Worksheet.fromJsObject).toList();
+      super.jsObject.items.map(Worksheet.getInstance).toList();
 
   office_extension.ClientResult getCount({final bool? visibleOnly}) =>
-      office_extension.ClientResult.fromJsObject(
+      office_extension.ClientResult.getInstance(
         super.jsObject.getCount(visibleOnly),
       );
   Worksheet getFirst({final bool? visibleOnly}) =>
-      Worksheet.fromJsObject(super.jsObject.getFirst(visibleOnly));
+      Worksheet.getInstance(super.jsObject.getFirst(visibleOnly));
 
   Worksheet getItem(final String key) =>
-      Worksheet.fromJsObject(super.jsObject.getItem(key));
+      Worksheet.getInstance(super.jsObject.getItem(key));
 }
 
 class Worksheet extends JsObjectWrapper<excel_js.WorksheetJsImpl> {
-  Worksheet.fromJsObject(super.jsObject);
+  Worksheet._fromJsObject(super.jsObject);
+
+  /// Creates a [Worksheet] from a [jsObject].
+  ///
+  /// {@macro expando_explanation}
+  factory Worksheet.getInstance(
+    final excel_js.WorksheetJsImpl jsObject,
+  ) {
+    return _expando[jsObject] ??= Worksheet._fromJsObject(jsObject);
+  }
+  static final _expando = Expando<Worksheet>();
 
   RequestContext get context =>
-      RequestContext.fromJsObject(super.jsObject.context);
+      RequestContext.getInstance(super.jsObject.context);
 
   String get name => super.jsObject.name;
 
   Worksheet load(final List<String> propertyNames) =>
-      Worksheet.fromJsObject(super.jsObject.load(propertyNames));
+      Worksheet.getInstance(super.jsObject.load(propertyNames));
 }
--- lib/src/office/office_extension.dart
+++ lib/src/office/office_extension.dart
@@ -7,7 +7,17 @@ import '../utils/interpop_utils.dart';
 
 class ClientRequestContext
     extends JsObjectWrapper<office_extension_js.ClientRequestContextJsImpl> {
-  ClientRequestContext.fromJsObject(super.jsObject);
+  ClientRequestContext._fromJsObject(super.jsObject);
+
+  /// Creates a [ClientRequestContext] from a [jsObject].
+  ///
+  /// {@macro expando_explanation}
+  factory ClientRequestContext.getInstance(
+    final office_extension_js.ClientRequestContextJsImpl jsObject,
+  ) {
+    return _expando[jsObject] ??= ClientRequestContext._fromJsObject(jsObject);
+  }
+  static final _expando = Expando<ClientRequestContext>();
 
   void load(
     final ClientObject object,
@@ -23,17 +33,38 @@ class ClientRequestContext
 
 class ClientObject
     extends JsObjectWrapper<office_extension_js.ClientObjectJsImpl> {
-  ClientObject.fromJsObject(super.jsObject);
+  ClientObject._fromJsObject(super.jsObject);
+
+  /// Creates a [ClientObject] from a [jsObject].
+  ///
+  /// {@macro expando_explanation}
+  factory ClientObject.getInstance(
+    final office_extension_js.ClientObjectJsImpl jsObject,
+  ) {
+    return _expando[jsObject] ??= ClientObject._fromJsObject(jsObject);
+  }
+  static final _expando = Expando<ClientObject>();
 
   ClientRequestContext get context =>
-      ClientRequestContext.fromJsObject(super.jsObject.context);
+      ClientRequestContext.getInstance(super.jsObject.context);
 
   bool get isNullObject => super.jsObject.isNullObject;
 }
 
 class ClientResult<T>
     extends JsObjectWrapper<office_extension_js.ClientResultJsImpl<T>> {
-  ClientResult.fromJsObject(super.jsObject);
+  ClientResult._fromJsObject(super.jsObject);
+
+  /// Creates a [ClientResult] from a [jsObject].
+  ///
+  /// {@macro expando_explanation}
+  factory ClientResult.getInstance(
+    final office_extension_js.ClientResultJsImpl<T> jsObject,
+  ) {
+    return (_expando[jsObject] ??= ClientResult._fromJsObject(jsObject))
+        as ClientResult<T>;
+  }
+  static final _expando = Expando<ClientResult<dynamic>>();
 
   T get value => super.jsObject.value;
 }
