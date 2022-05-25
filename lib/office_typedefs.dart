8851ba6f68e902060a9997ce1976d4074f72a652
Anton Malofeev <anton@xsoulspace.dev>
2022-05-25 18:29:59 +0300

    fix: types

--- lib/office_typedefs.dart
+++ lib/office_typedefs.dart
@@ -1,10 +1,6 @@
-library officejs;
+library officejs_typedefs;
 
-export './src/abstract/js_object_wrapper.dart';
-export './src/office/excel.dart';
 export './src/office/models/excel_models.dart';
 export './src/office/models/excel_typedefs.dart';
 export './src/office/models/office_models.dart';
 export './src/office/models/office_typedefs.dart';
-export './src/office/office.dart';
-export './src/office/office_extension.dart';
